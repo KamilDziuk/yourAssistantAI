a4e44b40466bf61a7ca13cd97edc06e862c3aa16
kamildziuk <kamildziukwebdeveloper@gmail.com>
2026-03-22 23:48:43 +0100

    feat: add axios form submission logic
    Extend hook with sendData handler to send data and manage UI state after submission.

--- client/src/components/AgentConfiguration/formBehavior.ts
+++ client/src/components/AgentConfiguration/formBehavior.ts
@@ -6,13 +6,38 @@ export function useMenuBehaviorAfterSending() {
   const [successfulSending, setSuccessfulSending] = useState(true);
   const [sendingError, setSendingError] = useState(true);
 
+  // Function variable for sending data from the form
+  const sendData = (e: any) => {
+    e.preventDefault();
 
-  
+    // Sending form data to the /contact endpoint using axios
+    axios
+      .post("http://localhost:3003/contact", {
+        clientGuidelines,
+      })
+      .then(() => {
+        // Display success message on page after 2s show form
+        setTimeout(() => {
+          setSuccessfulSending(true);
+        }, 4000);
+        setSuccessfulSending(false);
+        setSendingError(false);
+      })
+      .catch((err) => {
+        // Display failure message on page after 2s show form
+        setTimeout(() => {
+          setSuccessfulSending(true);
+        }, 2000);
+        setSuccessfulSending(false);
+        setSendingError(true);
+        console.error("error", err.message);
+      });
+  };
   // Returning constant variables from the menuBehaviorAfterSending function
   return {
     successfulSending,
     sendingError,
-    sendMail,
+    sendData,
     clientGuidelines,
     setClientGuidelines,
   };
