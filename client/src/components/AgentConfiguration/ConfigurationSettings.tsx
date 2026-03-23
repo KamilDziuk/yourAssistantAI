d3f1b537895b6e33571638d7b746d2f3b2c914f7
kamildziuk <kamildziukwebdeveloper@gmail.com>
2026-03-23 15:58:41 +0100

    feat: add configuration settings component
    Initialize component with required imports and base setup.

---
+++ client/src/components/AgentConfiguration/ConfigurationSettings.tsx
@@ -0,0 +1,11 @@
+
+// import Button from "../Button/Button";
+import { motion as Motion} from "framer-motion";
+import { useMenuBehaviorAfterSending } from "./formBehavior";
+import fromStyle from "../ChatWindow/ChatWindow.module.css";
+import iconStyle from "../Icon/Icon.module.css";
+import Icon from "../Icon/Icon";
+export default function ConfigurationSettings() {
+
+
+}
