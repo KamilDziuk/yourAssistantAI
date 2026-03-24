071d4e99787710475406244d90e4acd25205831f
kamildziuk <kamildziukwebdeveloper@gmail.com>
2026-03-24 18:01:02 +0100

    feat: add routes for Home and AgentConfiguration
    Define routes for / and /agent-configuration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,12 @@ import Home from "../src/pages/Home";
 import AgentConfiguration from "../src/pages/AgentConfiguration";
 function App() {
   return (
-
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/agent-configuration" element={<AgentConfiguration />} />
+      </Routes>
+    </Router>
   );
 }
 export default App;
