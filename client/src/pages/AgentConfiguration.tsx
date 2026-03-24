1860148c2aba966fca0ef27b9f669c20c441a24a
kamildziuk <kamildziukwebdeveloper@gmail.com>
2026-03-24 17:58:02 +0100

    feat: add AgentConfiguration page
    Render ConfigurationSettings component

---
+++ client/src/pages/AgentConfiguration.tsx
@@ -0,0 +1,9 @@
+import ConfigurationSettings from "../components/AgentConfiguration/ConfigurationSettings";
+
+export default function AgentConfiguration() {
+  return (
+    <>
+      <ConfigurationSettings />
+    </>
+  );
+}
